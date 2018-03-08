8c8c80f6075d5896e8644a93fd8f71a6d0b66031
flatlinebb <flatlinebb@gmail.com>
2018-03-07 19:35:28 -0800

    Update Run_WUInstall_On_Boot.ps1
    Added option to hide Preview updates.

--- Run_WUInstall_On_Boot.ps1
+++ Run_WUInstall_On_Boot.ps1
@@ -11,12 +11,31 @@ if ( !$Show )
     return
 }
 
-#Write-Host "Hiding junk updates for Bing and Skype ..."
-#Hide-WUUpdate -Title "Bing*,Skype*" -MicrosoftUpdate -Confirm:$false
+##
+# Put this in the shortut in shell:StartUp
+# C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup
+# C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -Command "C:\Users\Administrator\Downloads\wuinstall.ps1"
+# Start in: C:\Users\Administrator\Downloads
+##
+
+param ( $Show )
+if ( !$Show ) 
+{
+    PowerShell -NoExit -File $MyInvocation.MyCommand.Path 1
+    return
+}
+
+Add-WUServiceManager -ServiceID 7971f918-a847-4430-9279-4a52d1efe18d -Confirm:$false
+
+Write-Host "Hiding junk updates for Bing and Skype ..."
+Hide-WUUpdate -Title "Bing*,Skype*,Internet*" -MicrosoftUpdate -Confirm:$false
 Write-Host "Hiding MS Essentials updates: KB3140479,KB2902907,KB2876229  ...."
 Hide-WUUpdate -KBArticleID "KB3140479,KB2902907,KB2876229,KB2673774,KB2673773,KB2673772,KB2673771,KB2673770,KB2626808,KB2626806,KB2626807,KB2626804,KB2617376" -MicrosoftUpdate -Confirm:$false
 Write-Host "Hiding Driver updates: ..."
 Hide-WUUpdate -UpdateType Driver -MicrosoftUpdate -Confirm:$false
+
+Write-Host "Hiding Preview Updates: ..."
+Hide-WUUpdate -Title "Preview*" -MicrosoftUpdate -Confirm:$false
 
 Write-Host "Starting Updates ..."
 Get-WUInstall -MicrosoftUpdate -AutoReboot -AcceptAll -Confirm:$false
