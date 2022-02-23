26e141544a97dcfaa7f08460875a4d022803ed1f
flatlinebb <4769578+flatlinebb@users.noreply.github.com>
2022-02-23 09:39:48 -0800

    Update Run_WUInstall_On_Boot.ps1

--- Run_WUInstall_On_Boot.ps1
+++ Run_WUInstall_On_Boot.ps1
@@ -1,33 +1,39 @@
 ##
-# Put this in a shortcut in 'shell:StartUp'
-# C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup
-# C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -Command "C:\Users\Administrator\Downloads\wuinstall.ps1"
-# Start in: C:\Users\Administrator\Downloads
+# Put this in a shortut in "shell:StartUp" (C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup)
+#    C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoLogo -ExecutionPolicy Bypass -Command "C:\Support\Run_WUInstall_On_Boot.ps1"
+#    Start in: C:\Support
+##
+# Or run from a separate batch file located in the same folder as the PS script:
+#    @ECHO OFF
+#    powershell.exe -NoLogo "Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -File "%~dp0Run_WUInstall_On_Boot.ps1"' -Verb RunAs"
+#    exit
 ##
 
 param ( $Show )
 if ( !$Show ) 
 {
-    PowerShell -NoExit -File $MyInvocation.MyCommand.Path 1
+    PowerShell -NoLogo -NoExit -File $MyInvocation.MyCommand.Path 1
     return
 }
 
-Add-WUServiceManager -ServiceID 7971f918-a847-4430-9279-4a52d1efe18d -Confirm:$false
+Write-Host "Starting Windows Update process. Please wait for it to complete ..."
 
 Write-Host "Hiding junk updates for Bing and Skype ..."
-Hide-WUUpdate -Title "Bing*,Skype*,Internet*" -MicrosoftUpdate -Confirm:$false
-Write-Host "Hiding MS Essentials updates: KB3140479,KB2902907,KB2876229  ...."
-Hide-WUUpdate -KBArticleID "KB3140479,KB2902907,KB2876229,KB2673774,KB2673773,KB2673772,KB2673771,KB2673770,KB2626808,KB2626806,KB2626807,KB2626804,KB2617376" -MicrosoftUpdate -Confirm:$false
-Write-Host "Hiding Driver updates: ..."
-Hide-WUUpdate -UpdateType Driver -MicrosoftUpdate -Confirm:$false
+Hide-WindowsUpdate -Title "Bing*,Skype*" -MicrosoftUpdate -Confirm:$false
 
-Write-Host "Hiding Preview Updates: ..."
-Hide-WUUpdate -Title "Preview*" -MicrosoftUpdate -Confirm:$false
+# Only included here as a smaple on hopw to hide specific KB's:
+# Write-Host "Hiding MS Essentials updates: KB3140479,KB2902907,KB2876229  ...."
+# Hide-WindowsUpdate -KBArticleID "KB2876229,KB2673774,KB2673773,KB2673772,KB2673771,KB2673770,KB2626808,KB2626806,KB2626807,KB2626804,KB2617376" -MicrosoftUpdate -Confirm:$false
 
-Write-Host "Starting Updates ..."
-Get-WUInstall -MicrosoftUpdate -AutoReboot -AcceptAll -Confirm:$false
+Write-Host "Registering Microsoft Update Service ..."
+Add-WUServiceManager -ServiceID "7971f918-a847-4430-9279-4a52d1efe18d" -Confirm:$false
+
+Write-Host "Starting Updates Install ..."
+Install-WindowsUpdate -MicrosoftUpdate -RecurseCycle 3 -AcceptAll -AutoReboot -Confirm:$false
 
 ###
+# Microsoft Security Essentials KB3140479
+# Microsoft Security Essentials KB2902907
 # Bing Bar 7.1 KB2673774
 # Bing Bar 7.1 KB2673773
 # Bing Bar 7.1 KB2673772
